8b74517beb81c6eae048c0fabdebeb7fe9d12ed9
toystack-demo <toystack1@gmail.com>
2024-12-05 09:18:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!I am new python application!!Welcome to Toystack!'
+    return 'Hello, World! I am a demo application!!Welcome to Toystack!'
 
 if __name__ == '__main__':
     app.run(debug=True)
